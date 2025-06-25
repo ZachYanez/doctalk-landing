cd9632167a9adf7dbba0792de2240ba778208a0c
Zachary Yanez <zachyanez@gmail.com>
2025-06-25 01:23:04 -0500

    fix: configure for GitHub Pages static hosting

--- next.config.js
+++ next.config.js
@@ -1,8 +1,12 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  output: "export",
   images: {
     unoptimized: true,
   },
+  // This is needed for GitHub Pages
+  basePath: "",
+  assetPrefix: "",
 };
 
 module.exports = nextConfig;
